685305a203010f4416653aa9a031e4a50ecd8ace
Lindsay Cade <lindsay.cade@ucsf.edu>
2014-04-02 16:39:42 -0700

    initial commit

---
+++ cbio_utils.R
@@ -0,0 +1,118 @@
+library(cgdsr)
+library(stringr)
+library(plyr)
+
+
+#Wrappers for base functions
+#Brief documentation for base functions here: http://www.cbioportal.org/public-portal/cgds_r.jsp
+
+cbio_get_tcga_studies = function(con, cancer_type){
+  studies= getCancerStudies(con) #all studies
+  tcga_cancer_type = paste(cancer_type, 'tcga', sep='_')
+  tcga_studies_for_cancer_type = studies[ str_detect(studies$cancer_study_id, tcga_cancer_type) , 'cancer_study_id']
+  return(tcga_studies_for_cancer_type)
+}
+
+cbio_get_sample_set_id = function(con, study_id, case_type){
+  cases = getCaseLists(con, study_id)
+  single_case_id = cases[ str_detect(cases$case_list_id, case_type), 'case_list_id']
+  return(single_case_id)
+}
+
+cbio_get_sample_sets = function(con, study_id){
+  cases = getCaseLists(con, study_id)[1:4]
+  return(cases)
+}
+
+cbio_get_profile_id = function(con, study_id, profile_type){
+  profiles = getGeneticProfiles(con, study_id)
+  single_profile_id = profiles[ str_detect(profiles$genetic_profile_id, profile_type), 'genetic_profile_id']
+  return(single_profile_id)
+}
+
+cbio_base_fetch_data = function(con, study_id, data_type, genes, case_type = 'tcga_all'){
+# cbio_base_fetch_data = function(con, study_id, data_type, genes, case_type = '3way_complete'){
+  case_id = cbio_get_sample_set_id(con, study_id, case_type)
+  profile_id = cbio_get_profile_id(con, study_id, data_type)
+  d = getProfileData(con, genes, profile_id, case_id)[-1,]
+  return(d)
+
+}
+
+
+# Wrappers for specific data types from cbio_portal
+cbio_clinical = function(con, study_id, case_type = 'tcga_all'){
+  case_id = cbio_get_sample_set_id(con, study_id, case_type)
+  clin = getClinicalData(con, case_id)
+  return(clin)
+}
+
+cbio_rppa = function(con, study_id, abs){
+  #TODO find some way to validate gene/antibody names.
+  rppa_df = cbio_base_fetch_data(con, study_id, 'RPPA', abs)
+  colnames(rppa_df) = paste(colnames(rppa_df), 'prot', sep='_')
+  return(rppa_df)
+}
+
+cbio_muts = function(con, study_id, genes){
+  mut_df = cbio_base_fetch_data(con, study_id, 'mutations', genes)
+  colnames(mut_df) = paste(colnames(mut_df), 'mut', sep='_')
+  return(mut_df)
+}
+
+cbio_mrna = function(con, study_id, genes){
+  mrna_df = cbio_base_fetch_data(con, study_id, 'rna_seq_v2_mrna$', genes)
+  colnames(mrna_df) = paste(colnames(mrna_df), 'rsem', sep='_')
+  return(mrna_df)
+}
+
+
+# Compiling all datatypes
+cbio_fetch_all = function(cancer_type, genes, ab_names, study_id_num=1){
+  source('tcga_utils.R')
+  cbio_con =  make_cbio_connection()
+  study_id = cbio_get_tcga_studies(cbio_con, cancer_type)
+  study_id = multiple_study_ids_warning(cancer_type, study_id, study_id_num)
+  possible_cases = cbio_get_sample_sets(con, study_id)
+
+  rppa = add_tcga_id_col(cbio_rppa(cbio_con, study_id, ab_names))
+  muts = add_tcga_id_col(cbio_muts(cbio_con, study_id, genes))
+  mrna = add_tcga_id_col(cbio_mrna(cbio_con, study_id, genes))
+  clin = add_tcga_id_col(cbio_clinical(cbio_con, study_id))
+  check_clin(clin, study_id)
+
+  a = join_all(list(rppa, muts, mrna, clin), by='tcga_id')
+  a$tcga_id = format_tcga_barcodes(a$tcga_id)
+
+  protein_antibody_name_warning()
+  return(a)
+
+}
+
+# Helper functions
+
+make_cbio_connection = function(){
+  return(CGDS("http://www.cbioportal.org/public-portal/"))
+}
+
+add_tcga_id_col = function(d){
+  d$tcga_id = rownames(d)
+  return(d)
+}
+
+multiple_study_ids_warning = function(cancer_type, study_id, study_id_num){
+  if(length(study_id) > 1){
+    print(paste('Warning: there is more than one tcga study for', cancer_type, '. [', paste(study_id, collapse=', '), ']. Number', study_id_num, 'was chosen.'))
+  }
+  return(study_id[study_id_num])
+}
+
+protein_antibody_name_warning = function(){
+  warning('if your protein data is incomplete, check http://bit.ly/QGLSvD for possible antibody/gene names.')  
+}
+
+check_clin = function(clin, study_id){
+  if(length(clin) == 0){
+    warning(paste('There was no clinical data from cbio for', study_id, '.'))
+  }
+}
---
+++ tcga_utils.R
@@ -0,0 +1,92 @@
+tcga_format_barcodes = function(barcodes){
+  # replaces periods with dashes
+  # removes all characters after the patient barcode
+  library(stringr)
+  barcodes = gsub('.', '-', barcodes, fixed=T)
+  barcodes = str_sub(barcodes, 1, 12)
+  return(barcodes)
+}
+
+
+tcga_combine_death_and_last_followup = function(d, l){
+  # d = column listing days_to_death
+  # l = column listing days_to_last_followup
+  n = vector()
+  for(i in 1:length(d)){
+    #if both are na, return NA
+    if(is.na(d[i]) & is.na(l[i])){
+      n[i] = NA
+    }
+    #if only last followup is listed, return that one
+    else if(is.na(d[i]) & !is.na(l[i])){
+      n[i] = l[i]
+    }
+    # if only death is listed, return that one
+    else if(!is.na(d[i]) & is.na(l[i])){
+      n[i] = d[i]
+    }
+    # if both are listed
+    else if(!is.na(d[i]) & !is.na(l[i])){
+      #if they are both equal, return last followup
+      if(d[i] == l[i]){
+        n[i] = l[i]
+      }
+      #otherwise return days to death
+      else{
+        print(i)
+        print(paste(d[i], l[i]))
+        n[i] = d[i]
+      }
+    }
+  }
+  return(n)
+}
+
+# removes strings from days_to_* columns and makes it numeric
+tcga_clean_days_to_cols = function(column){
+  library(stringr)
+  column = str_replace(column, '[Not Applicable]', NA)
+  column = str_replace(column, '[Not Available]', NA)
+  column = as.numeric(column)
+  return(column)
+}
+
+#consolidateds clinical stage notations
+tcga_pathologic_stage_cleaned = function(stage_col){
+  cleaned = sapply(stage_col, function(x){
+    if(x == 'Stage I' | x == 'Stage IA' | x == 'Stage IB'){
+      return('1')
+    }
+    else if(x == 'Stage II' | x == 'Stage IIA' | x == 'Stage IIB' | x == 'Stage IIC' |
+            x == 'T2a' | x == 'T2b' | x == 'T2c'){
+      return('2')
+    }
+    else if(x == 'Stage III' | x == 'Stage IIIA' | x == 'Stage IIIB' | x == 'Stage IIIC' |
+            x == 'T3a' | x == 'T3b'){
+      return('3')
+    }
+    else if(x == 'Stage IV' | x == 'T4'){
+      return('4')
+    }
+    else{
+      return(NA)
+    }
+  })
+  return(cleaned)
+}
+
+#for use in kaplan meier plots
+high_or_low = function(cutoff, margin, vec){
+  h_o_l = sapply(vec, function(x){
+    if(x >= cutoff + margin){
+      return('high')
+    }
+    else if( x < cutoff - margin){
+      return('low')
+    }
+    else{
+      return(NA)
+    }
+  })
+  return(h_o_l)
+}
