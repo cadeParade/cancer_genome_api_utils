1e7f9a089f2fd5099e88aa55c28a4241d225efa7
Rachel <rachel.rosenthal@ucsf.edu>
2014-05-19 15:54:10 -0700

    combined function that made starter table with one that adds to table into one function called add_table

--- compile_all_exp_data_for_cancer_type.R
+++ compile_all_exp_data_for_cancer_type.R
@@ -33,25 +33,10 @@ find_tcga_bc = function(mage_tab, unc_id){
   return(tcga_bc_short)
 }
 
-add_table = function(compiled_table, table_filename, exp_col_name, mage_tab){
-  #reads expression data from each file, adds it to compiled table. 
-  df = read.tsv(table_filename)
-  df = df[,c('gene_id', exp_col_name)]
-  unc_id = extract_unc_id_from_filename(table_filename)
-  tcga_bc = find_tcga_bc(mage_tab, as.character(unc_id))
-  
-  if(tcga_bc == 'x'){
-    print(paste("UNC barcode", unc_id, "was not found in mage_tab file. It was left out of the table."))
-    return(compiled_table)}
-  colnames(df) = c('gene_id', tcga_bc)
-  compiled_table = merge(compiled_table, df, by='gene_id')
-  return(compiled_table)
-}
-
 file_loc_list = function(base_dir, cancer_type, file_type, out_loc){
-  file_locs = list( base_dir = paste('/taylorlab/data/tcga/tcga_rna/cancer_type/', cancer_type, sep=''))
-  file_locs$gene_exp_files = list.files(file_locs$base_dir, pattern = file_type, full.names=T, recursive=T)
-  file_locs$mage_tab_loc = list.files(file_locs$base_dir, pattern = 'sdrf.txt', full.names=T, recursive=T)
+  file_locs = list( dir = paste(base_dir, cancer_type, sep=''))
+  file_locs$gene_exp_files = list.files(file_locs$dir, pattern = file_type, full.names=T, recursive=T)
+  file_locs$mage_tab_loc = list.files(file_locs$dir, pattern = 'sdrf.txt', full.names=T, recursive=T)
   file_locs$out_file = out_loc
   return(file_locs)
 }
@@ -62,17 +47,6 @@ check_exp_type = function(file_type){
   else{ stop('file type must be either "rsem.genes.results" or "rsem.genes.normalized_results".') }
 }
 
-make_starter_table = function(file_locs, mage_tab, exp_col_name){
- #make starter table to add all the rest onto
-  all_gene_exp = read.tsv(file_locs$gene_exp_files[1])
-  all_gene_exp = all_gene_exp[,c('gene_id', exp_col_name)] 
-  
-  first_unc_id = extract_unc_id_from_filename(file_locs$gene_exp_files[1])
-  first_tcga_bc = find_tcga_bc(mage_tab, first_unc_id)
-  colnames(all_gene_exp) = c('gene_id', first_tcga_bc)
-  return(all_gene_exp)
-}
-
 remove_num_from_gene_id = function(gene_id_col){
   # removes number ID from gene_id column (they are originally like ABC123|20394 )
   return( unlist(sapply(gene_id_col, function(x){
@@ -80,16 +54,35 @@ remove_num_from_gene_id = function(gene_id_col){
   )
 }
 
-compile_sample_data = function(cancer_type, out_loc, file_type = 'rsem.genes.results', return_table=F){
+add_table = function(file_locs, mage_tab, exp_col_name){
+  all_gene_exp = data.frame()
+  for(f in 1:length(file_locs$gene_exp_files)){
+    tmp = read.tsv(file_locs$gene_exp_files[f])
+    tmp = tmp[,c('gene_id', exp_col_name)]
+    unc_id = extract_unc_id_from_filename(file_locs$gene_exp_files[f])
+    tcga_bc = find_tcga_bc(mage_tab, as.character(unc_id))
+    if(tcga_bc == 'x'){
+      print(paste("UNC barcode", unc_id, "was not found in mage_tab file.  It was left out of the table."))
+      return(all_gene_exp)
+    }
+    colnames(tmp) = c('gene_id', tcga_bc)
+    if(length(all_gene_exp) == 0){
+      all_gene_exp = tmp
+    }
+    else{
+      all_gene_exp = merge(all_gene_exp, tmp, by = 'gene_id')
+    }
+  }
+  return(all_gene_exp)
+}
+
+compile_sample_data = function(base_dir = '/taylorlab/data/tcga/tcga_rna/cancer_type/', cancer_type, out_loc, file_type = 'rsem.genes.results', return_table=F){
   
   file_locs = file_loc_list(base_dir, cancer_type, file_type, out_loc)
   exp_col_name = check_exp_type(file_type)
   mage_tab = read.tsv(file_locs$mage_tab_loc)
-  all_gene_exp = make_starter_table(file_locs, mage_tab, exp_col_name)
-  # compiles table, adds one column for each sample
-  for(f in file_locs$gene_exp_files[2:length(file_locs$gene_exp_files)]){
-    all_gene_exp = add_table(all_gene_exp, f, exp_col_name, mage_tab)
-  }
+  all_gene_exp = add_table(file_locs, mage_tab, exp_col_name)
+
   all_gene_exp$gene_id = remove_num_from_gene_id(all_gene_exp$gene_id)
   all_gene_exp = all_gene_exp[!(all_gene_exp$gene_id=='?'),]
   write.table(all_gene_exp, file_locs$out_file , sep='\t', row.names=F, quote=F)
